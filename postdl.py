9215e420788308bd3d3744f7da2b47b6aa797109
figsoda <figsoda@pm.me>
2020-10-13 15:17:06 -0400

    initial commit

---
+++ postdl.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import os, re, subprocess, sys
+
+def na(s): return None if s == "NA" else s
+
+name = sys.argv[1]
+
+m = re.fullmatch(r"(.+)@(.+)@(.+)@(.+)@(.+)@(.+)\.m4a", name)
+tags = subprocess.run(["AtomicParsley", name, "-t"], check = True, stdout = subprocess.PIPE, text = True).stdout
+
+track = na(m[1]) or m[2]
+
+try:
+    artist = next(re.finditer(r"Associated  Performer: (.*)", tags))[1]
+except StopIteration:
+    artist = na(m[3]) or na(m[4]) or na(m[5]) or m[6]
+
+subprocess.run(["AtomicParsley", name, "-W", "--artist", artist], check = True)
+os.rename(name, f"{track} - {artist}.m4a")
